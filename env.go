893755a840c1eeb50081edb34474d2a34f434d1e
idugan100 <isaacd4444@gmail.com>
2024-04-28 15:53:19 -0400

    making Env struct values not exported

--- env.go
+++ env.go
@@ -8,24 +8,24 @@ import (
 
 /** This is the env parsing struct. It contains a FilePath variable which is the location of the env file you want to parse and a map of values that were parsed*/
 type Env struct {
-	FilePath string
-	Values   map[string]string
+	filePath string
+	values   map[string]string
 }
 
 /** The function will return a pointer to a new Env struct. The default path is ./.env and the default Values map is empty*/
 func NewEnv() *Env {
-	return &Env{FilePath: "./.env", Values: make(map[string]string)}
+	return &Env{filePath: "./.env", values: make(map[string]string)}
 }
 
 /** This function allows you to set a path to a custom .env file besides the default ./.env */
 func (e *Env) SetEnvPath(path string) {
-	e.FilePath = path
+	e.filePath = path
 }
 
 /** This function performs that parsing of your env file. If you want to set the path to the env file please use SetEnvPath before calling this function. */
 func (e *Env) ParseEnv() error {
 
-	contents, err := os.ReadFile(e.FilePath)
+	contents, err := os.ReadFile(e.filePath)
 	if err != nil {
 		return fmt.Errorf("error reading file %w", err)
 	}
@@ -37,7 +37,7 @@ func (e *Env) ParseEnv() error {
 		if strings.Contains(line, "=") {
 			res := strings.SplitN(line, "=", 2)
 			if len(res) == 2 {
-				e.Values[res[0]] = res[1]
+				e.values[res[0]] = res[1]
 			}
 		}
 	}
@@ -46,5 +46,5 @@ func (e *Env) ParseEnv() error {
 
 /** This function is how you access the parsed env values. If the value has not been set an empty string will be returned. */
 func (e *Env) GetEnvValue(key string) string {
-	return e.Values[key]
+	return e.values[key]
 }
