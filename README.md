966bb5e141c9c849d693d68e3812beaa3d20a70f
idugan100 <isaacd4444@gmail.com>
2024-04-28 16:02:35 -0400

    adding package level docs and adding documentation generation instructions to the readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # golang env file parsing package
-I created this package to handle all my env file parsing on my go projects
+I created this package to handle all my env file parsing on my go projects.
 
 This package can be installed with ```go get github.com/idugan100/env```
+
+You can use the ```go doc``` command to see the documentation for this package. You can learn more about this [here](https://pkg.go.dev/cmd/doc).
--- env.go
+++ env.go
@@ -1,3 +1,6 @@
+/*
+This package allows use to parse env files. By default it will parse the file ./.env, but you can also set a custom path to the env file. Values must be seperated by an equals sign and each value must be on a new line.
+*/
 package env
 
 import (
@@ -6,23 +9,23 @@ import (
 	"strings"
 )
 
-/** This is the env parsing struct. It contains a FilePath variable which is the location of the env file you want to parse and a map of values that were parsed*/
+/* This is the env parsing struct. It contains a FilePath variable which is the location of the env file you want to parse and a map of values that were parsed*/
 type Env struct {
 	filePath string
 	values   map[string]string
 }
 
-/** The function will return a pointer to a new Env struct. The default path is ./.env and the default Values map is empty*/
+/* The function will return a pointer to a new Env struct. The default path is ./.env and the default Values map is empty*/
 func NewEnv() *Env {
 	return &Env{filePath: "./.env", values: make(map[string]string)}
 }
 
-/** This function allows you to set a path to a custom .env file besides the default ./.env */
+/* This function allows you to set a path to a custom .env file besides the default ./.env */
 func (e *Env) SetEnvPath(path string) {
 	e.filePath = path
 }
 
-/** This function performs that parsing of your env file. If you want to set the path to the env file please use SetEnvPath before calling this function. */
+/* This function performs that parsing of your env file. If you want to set the path to the env file please use SetEnvPath before calling this function. */
 func (e *Env) ParseEnv() error {
 
 	contents, err := os.ReadFile(e.filePath)
@@ -44,7 +47,7 @@ func (e *Env) ParseEnv() error {
 	return nil
 }
 
-/** This function is how you access the parsed env values. If the value has not been set an empty string will be returned. */
+/* This function is how you access the parsed env values. If the value has not been set an empty string will be returned. */
 func (e *Env) GetEnvValue(key string) string {
 	return e.values[key]
 }
