d93e1350c8bd57f1404cb652abdd3169aec80616
idugan100 <isaacd4444@gmail.com>
2024-04-28 15:50:43 -0400

    adding documentation to functions

--- env.go
+++ env.go
@@ -6,19 +6,23 @@ import (
 	"strings"
 )
 
+/** This is the env parsing struct. It contains a FilePath variable which is the location of the env file you want to parse and a map of values that were parsed*/
 type Env struct {
 	FilePath string
 	Values   map[string]string
 }
 
+/** The function will return a pointer to a new Env struct. The default path is ./.env and the default Values map is empty*/
 func NewEnv() *Env {
 	return &Env{FilePath: "./.env", Values: make(map[string]string)}
 }
 
+/** This function allows you to set a path to a custom .env file besides the default ./.env */
 func (e *Env) SetEnvPath(path string) {
 	e.FilePath = path
 }
 
+/** This function performs that parsing of your env file. If you want to set the path to the env file please use SetEnvPath before calling this function. */
 func (e *Env) ParseEnv() error {
 
 	contents, err := os.ReadFile(e.FilePath)
@@ -40,6 +44,7 @@ func (e *Env) ParseEnv() error {
 	return nil
 }
 
+/** This function is how you access the parsed env values. If the value has not been set an empty string will be returned. */
 func (e *Env) GetEnvValue(key string) string {
 	return e.Values[key]
 }
